f30ff6563088acdcc4db711b283eb4439f289c8c
Samuel Sieb <samuel@sieb.net>
2021-03-18 22:51:40 -0700

    Handle Tuya multi-datapoint messages

--- esphome/components/tuya/tuya.cpp
+++ esphome/components/tuya/tuya.cpp
@@ -185,7 +185,7 @@ void Tuya::handle_command_(uint8_t command, uint8_t version, const uint8_t *buff
         this->init_state_ = TuyaInitState::INIT_DONE;
         this->set_timeout("datapoint_dump", 1000, [this] { this->dump_config(); });
       }
-      this->handle_datapoint_(buffer, len);
+      this->handle_datapoints_(buffer, len);
       break;
     case TuyaCommandType::DATAPOINT_QUERY:
       break;
@@ -234,76 +234,82 @@ void Tuya::handle_command_(uint8_t command, uint8_t version, const uint8_t *buff
   }
 }
 
-void Tuya::handle_datapoint_(const uint8_t *buffer, size_t len) {
-  if (len < 2)
-    return;
+void Tuya::handle_datapoints_(const uint8_t *buffer, size_t len) {
+  while (len >= 4) {
+    TuyaDatapoint datapoint{};
+    datapoint.id = buffer[0];
+    datapoint.type = (TuyaDatapointType) buffer[1];
+    datapoint.value_uint = 0;
 
-  TuyaDatapoint datapoint{};
-  datapoint.id = buffer[0];
-  datapoint.type = (TuyaDatapointType) buffer[1];
-  datapoint.value_uint = 0;
+    // drop update if datapoint is in ignore_mcu_datapoint_update list
+    bool skip = false;
+    for (auto i : this->ignore_mcu_update_on_datapoints_) {
+      if (datapoint.id == i) {
+        ESP_LOGV(TAG, "Datapoint %u found in ignore_mcu_update_on_datapoints list, dropping MCU update", datapoint.id);
+        skip = true;
+        break;
+      }
+    }
+    if (skip)
+      continue;
 
-  // drop update if datapoint is in ignore_mcu_datapoint_update list
-  for (auto i : this->ignore_mcu_update_on_datapoints_) {
-    if (datapoint.id == i) {
-      ESP_LOGV(TAG, "Datapoint %u found in ignore_mcu_update_on_datapoints list, dropping MCU update", datapoint.id);
+    size_t data_size = (buffer[2] << 8) + buffer[3];
+    const uint8_t *data = buffer + 4;
+    size_t data_len = len - 4;
+    if (data_size != data_len) {
+      ESP_LOGW(TAG, "invalid datapoint update");
       return;
     }
-  }
 
-  size_t data_size = (buffer[2] << 8) + buffer[3];
-  const uint8_t *data = buffer + 4;
-  size_t data_len = len - 4;
-  if (data_size != data_len) {
-    ESP_LOGW(TAG, "invalid datapoint update");
-    return;
-  }
-
-  switch (datapoint.type) {
-    case TuyaDatapointType::BOOLEAN:
-      if (data_len != 1)
+    switch (datapoint.type) {
+      case TuyaDatapointType::BOOLEAN:
+        if (data_len != 1)
+          return;
+        datapoint.value_bool = data[0];
+        break;
+      case TuyaDatapointType::INTEGER:
+        if (data_len != 4)
+          return;
+        datapoint.value_uint = encode_uint32(data[0], data[1], data[2], data[3]);
+        break;
+      case TuyaDatapointType::STRING:
+        datapoint.value_string = std::string(reinterpret_cast<const char *>(data), data_len);
+        break;
+      case TuyaDatapointType::ENUM:
+        if (data_len != 1)
+          return;
+        datapoint.value_enum = data[0];
+        break;
+      case TuyaDatapointType::BITMASK:
+        if (data_len != 2)
+          return;
+        datapoint.value_bitmask = (uint16_t(data[0]) << 8) | (uint16_t(data[1]) << 0);
+        break;
+      default:
         return;
-      datapoint.value_bool = data[0];
-      break;
-    case TuyaDatapointType::INTEGER:
-      if (data_len != 4)
-        return;
-      datapoint.value_uint = encode_uint32(data[0], data[1], data[2], data[3]);
-      break;
-    case TuyaDatapointType::STRING:
-      datapoint.value_string = std::string(reinterpret_cast<const char *>(data), data_len);
-      break;
-    case TuyaDatapointType::ENUM:
-      if (data_len != 1)
-        return;
-      datapoint.value_enum = data[0];
-      break;
-    case TuyaDatapointType::BITMASK:
-      if (data_len != 2)
-        return;
-      datapoint.value_bitmask = (uint16_t(data[0]) << 8) | (uint16_t(data[1]) << 0);
-      break;
-    default:
-      return;
-  }
-  ESP_LOGV(TAG, "Datapoint %u update to %u", datapoint.id, datapoint.value_uint);
-
-  // Update internal datapoints
-  bool found = false;
-  for (auto &other : this->datapoints_) {
-    if (other.id == datapoint.id) {
-      other = datapoint;
-      found = true;
     }
-  }
-  if (!found) {
-    this->datapoints_.push_back(datapoint);
-  }
+    ESP_LOGV(TAG, "Datapoint %u update to %u", datapoint.id, datapoint.value_uint);
 
-  // Run through listeners
-  for (auto &listener : this->listeners_)
-    if (listener.datapoint_id == datapoint.id)
-      listener.on_datapoint(datapoint);
+    // Update internal datapoints
+    bool found = false;
+    for (auto &other : this->datapoints_) {
+      if (other.id == datapoint.id) {
+        other = datapoint;
+        found = true;
+      }
+    }
+    if (!found) {
+      this->datapoints_.push_back(datapoint);
+    }
+
+    // Run through listeners
+    for (auto &listener : this->listeners_)
+      if (listener.datapoint_id == datapoint.id)
+        listener.on_datapoint(datapoint);
+
+    len -= data_size + 4;
+    buffer = data + data_size;
+  }
 }
 
 void Tuya::send_raw_command_(TuyaCommand command) {
