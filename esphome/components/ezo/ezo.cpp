2316fbe083489a833c9d394160c383ac5fda90cd
Senex Crenshaw <senexcrenshaw@gmail.com>
2021-04-10 18:40:29 +0000

    Cleanup logging Added in codeowners

--- esphome/components/ezo/ezo.cpp
+++ esphome/components/ezo/ezo.cpp
@@ -55,7 +55,7 @@ void EZOSensor::loop() {
 
   if (!to_run->command_sent) {
     auto data = reinterpret_cast<const uint8_t *>(&to_run->command.c_str()[0]);
-    ESP_LOGD(TAG, "Sending command \"%s\"", data);
+    ESP_LOGVV(TAG, "Sending command \"%s\"", data);
 
     this->write_bytes_raw(data, to_run->command.length());
 
@@ -101,13 +101,13 @@ void EZOSensor::loop() {
       break;
   }
 
-  ESP_LOGD(TAG, "Received buffer \"%s\" for command type %s", buf, EzoCommandTypeStrings[to_run->command_type]);
+  ESP_LOGVV(TAG, "Received buffer \"%s\" for command type %s", buf, EzoCommandTypeStrings[to_run->command_type]);
 
-  for (int index = 0; index < 32; ++index) {
-    ESP_LOGD(TAG, "Received buffer index: %d char: \"%c\" %d", index, buf[index], buf[index]);
-  }
+  // for (int index = 0; index < 32; ++index) {
+  //   ESP_LOGD(TAG, "Received buffer index: %d char: \"%c\" %d", index, buf[index], buf[index]);
+  // }
 
-  if (buf[0] == 1 || (to_run->command_type == EzoCommandType::EZO_CALIBRATION)) {  // EZO_CALIBRATION returns 0-3
+  if ((buf[0] == 1) || (to_run->command_type == EzoCommandType::EZO_CALIBRATION)) {  // EZO_CALIBRATION returns 0-3
     // some sensors return multiple comma-separated values, terminate string after first one
     for (size_t i = 1; i < sizeof(buf) - 1; i++) {
       if (buf[i] == ',') {
@@ -156,9 +156,7 @@ void EZOSensor::loop() {
           this->t_callback_.call(payload);
           break;
         }
-        default: {
-          break;
-        }
+        default: { break; }
       }
     }
   }
