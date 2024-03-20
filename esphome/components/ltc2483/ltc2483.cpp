55fb5c5c057cc07f93701dab7f74de98936eecda
Samuel Sieb <samuel@sieb.net>
2024-03-19 19:30:52 -0700

    add ltc2483 adc

---
+++ esphome/components/ltc2483/ltc2483.cpp
@@ -0,0 +1,54 @@
+#include "ltc2483.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace ltc2483 {
+
+static const char *const TAG = "ltc2483";
+
+void LTC2483::dump_config() {
+  LOG_SENSOR("", "LTC2483 Sensor", this);
+  ESP_LOGCONFIG(TAG, "  VRef: %f", this->vref_);
+  LOG_UPDATE_INTERVAL(this);
+}
+
+float LTC2483::get_setup_priority() const { return setup_priority::DATA; }
+
+void LTC2483::update() {
+  uint8_t data[3];
+  this->read(data, 3);  // discard old data to start new conversion
+  this->updating_ = true;
+}
+
+void LTC2483::loop() {
+  if (!this->updating_)
+    return;
+  uint8_t data[3];
+  i2c::ErrorCode err = this->read(data, 3);
+  if (err == i2c::ERROR_NOT_ACKNOWLEDGED)
+    return;  // not ready
+  this->updating_ = false;
+  if (err != i2c::NO_ERROR) {
+    ESP_LOGE(TAG, "Error reading data: %d", err);
+    return;
+  }
+  int32_t raw = (data[2] << 16) | (data[1] << 8) | data[0];
+  if (((raw ^ (raw << 1)) & 0x800000) == 0) {
+    if (raw & 0x800000) {
+      ESP_LOGW(TAG, "voltage too high");
+    } else {
+      ESP_LOGW(TAG, "voltage too low");
+    }
+    this->publish_state(NAN);
+    return;
+  }
+  raw ^= 0x8000000;
+  if (raw & 0x800000)
+    raw |= 0xff000000;  // sign-extend the value
+  float value = float(raw) / 0x800000 * this->vref_;
+  ESP_LOGV(TAG, "Got voltage=%.4fV", value);
+  this->publish_state(value);
+}
+
+}  // namespace ltc2483
+}  // namespace esphome
---
+++ esphome/components/ltc2483/ltc2483.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/i2c/i2c.h"
+#include "esphome/components/sensor/sensor.h"
+
+namespace esphome {
+namespace ltc2483 {
+
+class LTC2483 : public sensor::Sensor, public i2c::I2CDevice, public PollingComponent {
+ public:
+  void update() override;
+  void loop() override;
+  void dump_config() override;
+  float get_setup_priority() const override;
+
+  void set_vref(float vref) { this->vref_ = vref; }
+
+ protected:
+  float vref_;
+  bool updating_{false};
+};
+
+}  // namespace ltc2483
+}  // namespace esphome
