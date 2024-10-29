70e640cd459f5170f4887237bf27111d5b0987f8
Samuel Sieb <samuel@sieb.net>
2024-10-29 01:34:45 -0700

    led direct control

--- esphome/components/lp5562/lp5562.cpp
+++ esphome/components/lp5562/lp5562.cpp
@@ -1,5 +1,6 @@
 #include "lp5562.h"
 
+#include "esphome/core/hal.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
@@ -8,6 +9,7 @@ namespace lp5562 {
 static const char *const TAG = "lp5562";
 static const uint8_t REG_ENABLE = 0;
 static const uint8_t REG_CONFIG = 8;
+static const uint8_t REG_LED_MAP = 0x70;
 
 void LP5562::dump_config() {
   ESP_LOGCONFIG(TAG, "LP5562:");
@@ -17,8 +19,11 @@ void LP5562::dump_config() {
 void LP5562::setup() {
   uint8_t c = 0xc0;
   this->write_register(REG_ENABLE, &c, 1);
+  delay(1);
   c = 0x43;
   this->write_register(REG_CONFIG, &c, 1);
+  c = 0;
+  this->write_register(REG_LED_MAP, &c, 1);
 }
 
 }  // namespace lp5562
